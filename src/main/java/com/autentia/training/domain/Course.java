65fcfb7c87a7f6d1b3ff0388cc1c8cc96c15ca9e
Angel Ruiz <aruizca@gmail.com>
2015-08-02 16:25:21 +1000

    Convert Course domain to Java

---
+++ src/main/java/com/autentia/training/domain/Course.java
@@ -0,0 +1,70 @@
+package com.autentia.training.domain;
+
+import java.io.Serializable;
+
+public class Course implements Serializable {
+    @Override
+    public String toString() {
+        return "Course{" + "id=" + id + ", active=" + active + ", teacher='" + teacher + "\'" + ", title='" + title + "\'" + ", hours=" + hours + ", level=" + level + "}";
+    }
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public Integer getLevel() {
+        return level;
+    }
+
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private Boolean active;
+    private String teacher;
+    private String title;
+    private Integer hours;
+    private Integer level;
+}
